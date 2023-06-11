3520974d1c8d3b4288c328518d52eaf69bcd8d52
Sayan De <sayandeten@gmail.com>
2023-06-11 15:59:15 +0530

    index files for components

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -3,6 +3,7 @@ export * from "./components/Button";
 export * from "./components/Header";
 export * from "./components/Footer";
 export * from "./components/Loaders";
+export * from "./components/Separator";
+export * from "./components/Tooltip";
 export * from "./forms";
-export * from "./components/Tooltip/tooltip";
 export * from "./typography";
