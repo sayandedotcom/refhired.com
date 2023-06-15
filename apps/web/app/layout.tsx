1088aa0954dfe14e2e6bea97b04566d29a05fb26
Sayan De <sayandeten@gmail.com>
2023-06-16 03:23:03 +0530

    adding conditional layout

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,13 +1,32 @@
 "use client";
+
+import { usePathname } from "next/navigation";
+import { Footer, Header } from "@referrer/ui";
 import "../styles/globals.css";
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const pathName = usePathname();
+
   return (
     <html lang='en'>
-      <body>{children}</body>
+      <body>
+        {pathName === "/" ||
+        pathName === "/blogs" ||
+        pathName === "/docs" ||
+        pathName === "/about-us" ||
+        pathName === "/pricing" ? (
+          <>
+            <Header />
+            {children}
+            <Footer />
+          </>
+        ) : (
+          <>{children}</>
+        )}
+      </body>
     </html>
   );
 }
