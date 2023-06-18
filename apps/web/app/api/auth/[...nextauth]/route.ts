061d779669853e0f15ef94b795026d7cf078ebae
Sayan De <sayandeten@gmail.com>
2023-06-18 16:28:41 +0530

    google auth

--- apps/web/app/api/auth/[...nextauth]/route.ts
+++ apps/web/app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,17 @@
 import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
   providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
     CredentialsProvider({
       // The name to display on the sign in form (e.g. 'Sign in with...')
       name: "Credentials",
-      // The credentials is used to generate a suitable form on the sign in page.
+      // The credentials is used to generate a suicredentialstable form on the sign in page.
       // You can specify whatever fields you are expecting to be submitted.
       // e.g. domain, username, password, 2FA token, etc.
       // You can pass any HTML attribute to the <input> tag through the object.
@@ -21,7 +26,7 @@ const handler = NextAuth({
         // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
         // You can also use the `req` object to obtain additional parameters
         // (i.e., the request IP address)
-        const res = await fetch("/api/login", {
+        const res = await fetch("http://localhost:3000/api/login", {
           method: "POST",
           body: JSON.stringify({
             email: credentials?.email,
@@ -42,6 +47,7 @@ const handler = NextAuth({
   ],
   pages: {
     signIn: "/login",
+    signOut: "/",
   },
 });
 
