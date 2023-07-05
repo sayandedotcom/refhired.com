78f1492cd8884d207cf4f5f9d5c729bf12c71308
Sayan De <sayandeten@gmail.com>
2023-07-06 02:50:31 +0530

    search page

--- apps/web/app/(portals)/search/page.tsx
+++ apps/web/app/(portals)/search/page.tsx
@@ -1,21 +1,28 @@
 "use client";
 
-import { SlidersHorizontal } from "lucide-react";
-import { Input, Separator } from "@referrer/ui";
+import { Search, SlidersHorizontal } from "lucide-react";
+import { Button, Input, Separator } from "@referrer/ui";
 import { TooltipDemo } from "@/components/ui";
 
-const Search = () => {
+const SearchPage = () => {
   return (
     <>
-      <div className='flex items-center gap-1 md:gap-2 m-3 md:m-4 md:px-2'>
-        <Input
-          type='text'
-          placeholder='Search.....'
-          className='rounded-full w-11/12 bg-muted text-base md:text-lg'
-        />
+      <div className='sticky top-0 flex items-center gap-1 md:gap-2 m-2'>
+        <form className='flex w-full rounded-full overflow-hidden bg-transparent'>
+          <Input
+            type='text'
+            placeholder='Search.....'
+            className='bg-muted text-base md:text-lg rounded-none w-11/12'
+          />
+          <TooltipDemo text='Search'>
+            <Button size='icon' className='rounded-none w-1/12'>
+              <Search />
+            </Button>
+          </TooltipDemo>
+        </form>
         <TooltipDemo text='Filter'>
           <div className='rounded-full cursor-pointer hover:bg-muted'>
-            <SlidersHorizontal className='m-2 h-5 md:h-8' />
+            <SlidersHorizontal className='m-2 h-4 md:h-5' />
           </div>
         </TooltipDemo>
       </div>
@@ -24,4 +31,4 @@ const Search = () => {
   );
 };
 
-export default Search;
+export default SearchPage;
