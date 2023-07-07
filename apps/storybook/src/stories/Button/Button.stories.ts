3df81bdf3608cfe1eaf3d00c71fb1551fef23a6c
Sayan De <sayandeten@gmail.com>
2023-07-08 01:37:07 +0530

    push

--- apps/storybook/src/stories/Button/Button.stories.ts
+++ apps/storybook/src/stories/Button/Button.stories.ts
@@ -11,14 +11,7 @@ const meta: Meta<typeof Button> = {
     backgroundColor: { control: "color" },
     variant: {
       control: "select",
-      options: [
-        "default",
-        "destructive",
-        "outline",
-        "secondary",
-        "ghost",
-        "link",
-      ],
+      options: ["default", "destructive", "outline", "secondary", "ghost", "link"],
     },
     size: {
       control: "select",
