3ff65defe734a7b016b3cde46b7a3dc94ea60426
Sayan De <sayandeten@gmail.com>
2023-07-08 17:27:26 +0530

    adding eslint-plugin-unused-imports plugin

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,8 +1,15 @@
 module.exports = {
   extends: ["next", "turbo", "prettier"],
+  plugins: ["unused-imports"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/no-unescaped-entities": "warn",
+    "no-unused-vars": "off", // or "@typescript-eslint/no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "warn",
+    "unused-imports/no-unused-vars": [
+      "warn",
+      { vars: "all", varsIgnorePattern: "^_", args: "after-used", argsIgnorePattern: "^_" },
+    ],
   },
   parserOptions: {
     babelOptions: {
