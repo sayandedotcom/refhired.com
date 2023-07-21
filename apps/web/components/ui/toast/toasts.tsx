35f04b1cf969a3c02fb0719b2c23a6681021606f
Sayan De <sayandeten@gmail.com>
2023-07-21 11:13:24 +0530

    adding generic toast component

--- apps/web/components/ui/toast/toasts.tsx
+++ apps/web/components/ui/toast/toasts.tsx
@@ -1,19 +1,14 @@
 "use client";
 
+import { toastType } from "@/types/toast";
 import toast from "react-hot-toast";
 
 import { Alert } from "@referrer/ui";
 
 const TOAST_VISIBLE_DURATION = 3000;
 
-export const customToast = (
-  type: "success" | "warning" | "error" | "info" | "neutral",
-  title: string,
-  message?: string,
-  position?: "top-center" | "top-right" | "top-left" | "bottom-center" | "bottom-right" | "bottom-left",
-  duration?: number
-) =>
-  toast.custom((t) => <Alert severity={type} title={title} message={message} />, {
+export const toastMessage = ({ children, type, title, message, position, duration }: toastType) =>
+  toast.custom((t) => <>{children}</> ?? <Alert severity={type} title={title} message={message} />, {
     position: position ? position : "top-center",
     duration: duration ? duration : TOAST_VISIBLE_DURATION,
   });
