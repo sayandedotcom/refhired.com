241a647bd2ef8444ac460b7fb4ac66df1af02cc2
Sayan De <sayandeten@gmail.com>
2023-07-22 19:19:46 +0530

    adding settings page to redirect user to profile setting

---
+++ apps/web/app/(portals)/settings/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { useEffect } from "react";
+
+import { redirect } from "next/navigation";
+
+export default function Settings() {
+  useEffect(() => {
+    redirect("/settings/profile");
+  }, []);
+  return null;
+}
