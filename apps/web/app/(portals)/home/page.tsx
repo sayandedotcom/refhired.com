50a407d7c8c0894880c116817f2099fb976dd07f
Sayan De <sayandeten@gmail.com>
2023-07-22 14:49:59 +0530

    change post cards to react compound component

--- apps/web/app/(portals)/home/page.tsx
+++ apps/web/app/(portals)/home/page.tsx
@@ -2,21 +2,31 @@ import { Metadata } from "next";
 
 import { PostCard } from "@/components/custom-components";
 
+import AltImage from "../../../public/avatar/avatar.png";
+
 export const metadata: Metadata = {
   title: "Home",
   description: "Get job referrals to the top best companies of the world",
 };
-
+const tag = ["1-5 Years", "Full Time", "Front-End Developer", "150-250k", "JavaScript", "React"];
 const Home = () => {
   return (
     <>
-      <PostCard />
-      <PostCard />
-      <PostCard />
-      <PostCard />
-      <PostCard />
-      <PostCard />
-      <PostCard />
+      {[...new Array(30)].map((_, i) => (
+        <PostCard key={i}>
+          <PostCard.ProfileImage src={AltImage} />
+          <PostCard.PostCardContent>
+            <PostCard.PostCardHeader name="Full Name" userName="@username" time="1h ago" timeLeft="2d Left" />
+            <PostCard.PostCardDescription>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse, beatae vitae repellendus saepe
+              laudantium totam asperiores, nisi, repudiandae hic tenetur porro dolore consequuntur est. Beatae
+              ipsa nesciunt itaque expedita tempora?
+            </PostCard.PostCardDescription>
+            <PostCard.PostCardTags tag={tag} />
+            <PostCard.PostCardFooter />
+          </PostCard.PostCardContent>
+        </PostCard>
+      ))}
     </>
   );
 };
--- apps/web/components/custom-components/post-card/post-card.tsx
+++ apps/web/components/custom-components/post-card/post-card.tsx
@@ -6,18 +6,109 @@ import { Separator } from "@referrer/ui";
 
 import { PostHoverCard } from "@/components/ui";
 
-import AltImage from "../../../public/avatar/avatar.png";
 import { ApplyButton, MultipleButtons, Tags } from "./post-buttons";
 import { ComboboxDropdownMenu } from "./post-more-menu";
 
-const tag = ["1-5 Years", "Full Time", "Front-End Developer", "150-250k", "JavaScript", "React"];
-
-export const PostCard = () => {
+export const PostCard = ({ key, children }: { key: any; children: React.ReactNode }) => {
   return (
     <>
       <Separator />
-      <div className="mx-auto flex w-full gap-2 p-1 md:gap-3 md:p-4">
-        <div className="w-[12%]">
+      <div key={key} className="mx-auto flex w-full gap-2 p-1 md:gap-3 md:p-4">
+        {children}
+      </div>
+      <Separator />
+    </>
+  );
+};
+// (200 Applied)
+
+function ProfileImage({ src }: { src: any }) {
+  return (
+    <div className="w-[12%]">
+      <PostHoverCard>
+        <Image alt="img" src={src} width={64} height={64} className="cursor-pointer rounded-full" />
+      </PostHoverCard>
+    </div>
+  );
+}
+
+PostCard.ProfileImage = ProfileImage;
+
+function PostCardContent({ children }: { children: React.ReactNode }) {
+  return <div className="flex w-[88%] flex-col gap-2 md:w-full">{children}</div>;
+}
+
+PostCard.PostCardContent = PostCardContent;
+
+function PostCardHeader({
+  name,
+  userName,
+  time,
+  timeLeft,
+}: {
+  name: string;
+  userName: string;
+  time: string;
+  timeLeft: string;
+}) {
+  return (
+    <div className="flex items-center justify-between">
+      <div className="flex items-center gap-2 text-sm md:text-base">
+        <PostHoverCard>
+          <span className="cursor-pointer">{name}</span>
+        </PostHoverCard>
+        •
+        <PostHoverCard>
+          <span className="cursor-pointer font-thin dark:text-gray-200">{userName}</span>
+        </PostHoverCard>
+        •<small>{time}</small>
+        <small className="hidden md:block">• &nbsp; &nbsp;{timeLeft}</small>
+      </div>
+      <ComboboxDropdownMenu>
+        <div className="cursor-pointer rounded-full hover:bg-muted">
+          <MoreHorizontal className="w-5" />
+        </div>
+      </ComboboxDropdownMenu>
+    </div>
+  );
+}
+
+PostCard.PostCardHeader = PostCardHeader;
+
+function PostCardDescription({ children }: { children: React.ReactNode }) {
+  return (
+    <p className="cursor-pointer text-[15px] md:text-base">
+      {children}
+      <span className="float-right text-xs md:text-sm">....Show more</span>
+    </p>
+  );
+}
+
+PostCard.PostCardDescription = PostCardDescription;
+
+function PostCardTags({ tag }: { tag: string[] }) {
+  return (
+    <div className="flex h-5 gap-1">
+      <Tags tag={tag} />
+    </div>
+  );
+}
+
+PostCard.PostCardTags = PostCardTags;
+
+function PostCardFooter() {
+  return (
+    <div className="flex items-center justify-between">
+      <MultipleButtons />
+      <ApplyButton />
+    </div>
+  );
+}
+
+PostCard.PostCardFooter = PostCardFooter;
+
+{
+  /*    <div className="w-[12%]">
           <PostHoverCard>
             <Image alt="img" src={AltImage} width={64} height={64} className="cursor-pointer rounded-full" />
           </PostHoverCard>
@@ -54,10 +145,5 @@ export const PostCard = () => {
             <MultipleButtons />
             <ApplyButton />
           </div>
-        </div>
-      </div>
-      <Separator />
-    </>
-  );
-};
-// (200 Applied)
+        </div> */
+}
