7dec889d32b97a2421ef463c7758c2b350c01721
Sayan De <sayandeten@gmail.com>
2023-08-01 23:51:32 +0530

    config file

---
+++ apps/web/config/site.ts
@@ -0,0 +1,12 @@
+import { SiteConfig } from "@/types";
+
+export const siteConfig: SiteConfig = {
+  name: "Refhired.com",
+  description: "Get job referrals to the top best companies of the world.",
+  url: "https://tx.shadcn.com",
+  ogImage: "https://tx.shadcn.com/og.jpg",
+  links: {
+    twitter: "https://twitter.com/shadcn",
+    github: "https://github.com/shadcn/taxonomy",
+  },
+};
