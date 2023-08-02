0c17bbc5299ef92a33df12c7b5a1b4b46eb62110
Sayan De <sayandeten@gmail.com>
2023-08-02 21:11:14 +0530

    added middleware page

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,3 +1,5 @@
-export function middleware() {
-  console.log("middleware");
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  return console.log(request.url);
 }
