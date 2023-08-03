ab474b5d568905dd09958f309f945c33f608efd9
Sayan De <sayandeten@gmail.com>
2023-08-03 22:48:25 +0530

    pwa

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,7 @@
 const withPWA = require("next-pwa")({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
+  buildExcludes: ["app-build-manifest.json"],
 });
 
 module.exports = withPWA({
