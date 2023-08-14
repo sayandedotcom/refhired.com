05e3635decb3ee2f99a3995420b63d568e36b5de
Sayan De <sayandeten@gmail.com>
2023-08-15 02:30:21 +0530

    server actions types

---
+++ apps/web/types/server-actions.d.ts
@@ -0,0 +1,15 @@
+export type CREATEPOSTS = {
+  title?: string;
+  userId?: string;
+  description?: string;
+  expiresAt?: string;
+  role?: string;
+  jobType?: string;
+  experience?: number;
+  location?: string;
+  startingRange?: number;
+  endingRange?: number;
+  image?: string;
+  acceptingReferralsNo?: number;
+  normalPost?: boolean;
+};
