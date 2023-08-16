3f0fbb432de60aae03436cfe2095647797b0bdeb
Sayan De <sayandeten@gmail.com>
2023-08-16 22:41:45 +0530

    referal post

---
+++ apps/web/config/referral-post-items.ts
@@ -0,0 +1,24 @@
+export const jobTypeList = [
+  { value: "Full Time", label: "Full Time" },
+  { value: "Part Time", label: "Part Time" },
+  { value: "Intern", label: "Intern" },
+  { value: "Temporary", label: "Temporary" },
+  { value: "Contractor", label: "Contractor" },
+  { value: "Volunteer", label: "Volunteer" },
+  { value: "Freelance", label: "Freelance" },
+];
+
+export const items = [
+  {
+    id: "shortNote",
+    label: "Short Note",
+  },
+  {
+    id: "resume",
+    label: "Resume",
+  },
+  {
+    id: "coverLetter",
+    label: "Cover Letter",
+  },
+] as const;
