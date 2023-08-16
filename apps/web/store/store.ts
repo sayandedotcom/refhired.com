d787f3f137dc6489fbf5e5202757b1a4da1c28aa
Sayan De <sayandeten@gmail.com>
2023-08-16 21:07:30 +0530

    auth-dialog modal store

--- apps/web/store/store.ts
+++ apps/web/store/store.ts
@@ -4,17 +4,21 @@ type Store = {
   userName: string;
   postType: string;
   joyRide: string;
+  authDialogOpen: boolean;
   claimUserName: (userName: any) => void;
   setPostType: (postType: any) => void;
   setJoyRide: (joyRide: any) => void;
+  setAuthDialogOpen: (authDialogOpen: any) => void;
 };
 
 export const useStore = create<Store>((set) => ({
   userName: "",
   postType: null,
   joyRide: null,
+  authDialogOpen: null,
   // claimUserName: () => set((state) => ({ userName: state.userName })),
   claimUserName: (userName) => set({ userName }),
   setPostType: (postType) => set({ postType }),
   setJoyRide: (joyRide) => set({ joyRide }),
+  setAuthDialogOpen: (authDialogOpen) => set({ authDialogOpen }),
 }));
