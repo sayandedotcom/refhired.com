6af1220dad58a6f6a8f4c712edda0199a9738c8a
Sayan De <sayandeten@gmail.com>
2023-08-16 22:38:07 +0530

    layout

--- apps/web/app/(portals)/layout.tsx
+++ apps/web/app/(portals)/layout.tsx
@@ -4,12 +4,13 @@ import { useEffect, useState } from "react";
 
 import { usePathname } from "next/navigation";
 
-import { PostSteps, Steps } from "@/config";
+import { useSession } from "next-auth/react";
 import { useLocalStorage } from "usehooks-ts";
 
 import { Separator } from "@referrer/ui";
 
 import {
+  AuthDialog,
   NewContentSection,
   NewExtraSection, // ContentLargeSection,
   // ContentSection,
@@ -18,11 +19,16 @@ import {
   Walkthrough,
 } from "@/components/custom-components";
 
+import { PostSteps, Steps } from "@/config";
+
 import { useStore } from "@/store/store";
 
 import "../../styles/globals.css";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const { data: session } = useSession();
+  const setAuthDialogOpen = useStore((state) => state.setAuthDialogOpen);
+  setAuthDialogOpen(!session);
   const [run, setRun] = useState(false);
   const [showComponent, setShowComponent] = useState(false);
   const [countIntro, setCountIntro] = useLocalStorage("count-intro", 0);
@@ -37,17 +43,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   }, [showComponent, setShowComponent]);
 
   const showExtraSection = [
-    "/home",
-    "/search",
-    "/notifications",
-    "/bookmarks",
-    "/messages",
-    "/requests",
-    "/applied",
-    "/post",
+    "/dashboard",
+    "/settings/profile",
+    "/settings/appearance",
+    "/settings/notifications",
   ].includes(pathName);
-
-  const session = null;
 
   return (
     <>
@@ -55,20 +55,38 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Walkthrough steps={Steps} countIntro={countIntro} setCountIntro={setCountIntro} />
       )}
       <Walkthrough steps={PostSteps} run={!!joyRide} setRun={setRun} />
-      <section className="flex">
-        <NewOptionsSection session={session?.user?.userName ?? "profile"} />
-        <Separator orientation="vertical" className=" sticky top-0 h-screen dark:bg-[#2d3134]" />
-        <NewContentSection>{children}</NewContentSection>
-        <Separator orientation="vertical" className=" sticky top-0 h-screen dark:bg-[#2d3134]" />
-        {showExtraSection && <NewExtraSection />}
-        {/* <ContentSection width={showExtraSection ? 38 : 40}>{children}</ContentSection> */}
-        {/* {showExtraSection ? (
-        <ContentSection>{children}</ContentSection>
-      ) : (
-        <ContentLargeSection>{children}</ContentLargeSection>
-      )}
-      {showExtraSection && <ExtraSection />} */}
-      </section>
+      <AuthDialog>
+        <section className="flex">
+          <NewOptionsSection session={session?.user?.userName ?? "profile"} />
+          <Separator orientation="vertical" className=" sticky top-0 h-screen dark:bg-[#2d3134]" />
+          <NewContentSection>{children}</NewContentSection>
+          <Separator orientation="vertical" className=" sticky top-0 h-screen dark:bg-[#2d3134]" />
+          {showExtraSection ? null : <NewExtraSection />}
+        </section>
+      </AuthDialog>
     </>
   );
+}
+
+// const showExtraSection = [
+//   "/home",
+//   "/search",
+//   "/notifications",
+//   "/bookmarks",
+//   "/messages",
+//   "/requests",
+//   "/applied",
+//   "/post",
+// ].includes(pathName);
+
+{
+  /* <ContentSection width={showExtraSection ? 38 : 40}>{children}</ContentSection> */
+}
+{
+  /* {showExtraSection ? (
+    <ContentSection>{children}</ContentSection>
+    ) : (
+      <ContentLargeSection>{children}</ContentLargeSection>
+      )}
+{showExtraSection && <ExtraSection />} */
 }
