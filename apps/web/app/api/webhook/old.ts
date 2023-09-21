68f6ac2bef367d095616e3ea0368dbf7d4458145
Sayan De <sayandeten@gmail.com>
2023-09-22 01:10:14 +0530

    webhook error

---
+++ apps/web/app/api/webhook/old.ts
@@ -0,0 +1,65 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import Stripe from "stripe";
+
+import prisma from "@referrer/prisma";
+
+import { stripe } from "@/lib/stripe";
+
+const webhookSecret: string = process.env.STRIPE_WEBHOOK_SECRET;
+// ! const webhookHandler = async (req: NextRequest) => {
+export async function POST(req: NextRequest, res: any) {
+  try {
+    const buf = await req.text();
+    const sig = req.headers.get("stripe-signature")!;
+    let event: Stripe.Event;
+    // let event: any;
+    try {
+      event = stripe.webhooks.constructEvent(buf, sig, webhookSecret);
+    } catch (err) {
+      const errorMessage = err instanceof Error ? err.message : "Unknown error";
+      if (err! instanceof Error) console.log(err);
+      console.log(`❌ Error message: ${errorMessage}`);
+      return NextResponse.json(
+        {
+          error: {
+            message: `Webhook Error: ${errorMessage}`,
+          },
+        },
+        { status: 400 }
+      );
+    }
+    const { metadata } = event.data.object as Stripe.Checkout.Session;
+    switch (event.type) {
+      case "checkout.session.completed":
+        if (metadata?.userId && metadata?.stars) {
+          await prisma.user.update({
+            where: {
+              id: metadata.userId.toString(),
+            },
+            data: {
+              stars: {
+                increment: +metadata?.stars,
+              },
+            },
+          });
+        }
+        break;
+      default:
+        console.warn(`🤷‍♀️ Unhandled event type: ${event.type}`);
+        break;
+    }
+    // Return a response to acknowledge receipt of the event.
+    return NextResponse.json({ received: true });
+  } catch {
+    return NextResponse.json(
+      {
+        error: {
+          message: `Method Not Allowed`,
+        },
+      },
+      { status: 405 }
+    ).headers.set("Allow", "POST");
+  }
+}
+// ! export { webhookHandler as POST };
--- apps/web/app/api/webhook/route.ts
+++ apps/web/app/api/webhook/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+import { headers } from "next/headers";
 
 import Stripe from "stripe";
 
@@ -8,64 +8,34 @@ import { stripe } from "@/lib/stripe";
 
 const webhookSecret: string = process.env.STRIPE_WEBHOOK_SECRET;
 
-// ! const webhookHandler = async (req: NextRequest) => {
-export async function POST(req: NextRequest, res: any) {
+export async function POST(req: Request) {
+  const body = await req.text();
+  const signature = headers().get("Stripe-Signature") as string;
+
+  let event: Stripe.Event;
+
   try {
-    const buf = await req.text();
-    const sig = req.headers.get("stripe-signature")!;
-    let event: Stripe.Event;
-    // let event: any;
-    try {
-      event = stripe.webhooks.constructEvent(buf, sig, webhookSecret);
-    } catch (err) {
-      const errorMessage = err instanceof Error ? err.message : "Unknown error";
+    event = stripe.webhooks.constructEvent(body, signature, webhookSecret);
+  } catch (error) {
+    return new Response(`Webhook Error: ${error.message}`, { status: 400 });
+  }
 
-      if (err! instanceof Error) console.log(err);
-      console.log(`❌ Error message: ${errorMessage}`);
+  const { metadata } = event.data.object as Stripe.Checkout.Session;
 
-      return NextResponse.json(
-        {
-          error: {
-            message: `Webhook Error: ${errorMessage}`,
+  if (event.type === "checkout.session.completed") {
+    if (metadata?.userId && metadata?.stars) {
+      await prisma.user.update({
+        where: {
+          id: metadata.userId.toString(),
+        },
+        data: {
+          stars: {
+            increment: +metadata?.stars,
           },
         },
-        { status: 400 }
-      );
+      });
     }
-
-    const { metadata } = event.data.object as Stripe.Checkout.Session;
-    switch (event.type) {
-      case "checkout.session.completed":
-        if (metadata?.userId && metadata?.stars) {
-          await prisma.user.update({
-            where: {
-              id: metadata.userId.toString(),
-            },
-            data: {
-              stars: {
-                increment: +metadata?.stars,
-              },
-            },
-          });
-        }
-        break;
-
-      default:
-        console.warn(`🤷‍♀️ Unhandled event type: ${event.type}`);
-        break;
-    }
-    // Return a response to acknowledge receipt of the event.
-    return NextResponse.json({ received: true });
-  } catch {
-    return NextResponse.json(
-      {
-        error: {
-          message: `Method Not Allowed`,
-        },
-      },
-      { status: 405 }
-    ).headers.set("Allow", "POST");
   }
-}
 
-// ! export { webhookHandler as POST };
+  return new Response(null, { status: 200 });
+}
