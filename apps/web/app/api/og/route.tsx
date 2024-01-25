e98c0e051264b1fcebdafbff55396776b0ccd977
Sayan De <sayandeten@gmail.com>
2024-01-25 23:56:30 +0530

    dep

--- apps/web/app/api/og/route.tsx
+++ apps/web/app/api/og/route.tsx
@@ -1,4 +1,4 @@
-import { ImageResponse } from "next/server";
+// import { ImageResponse } from "next/server";
 
 export const runtime = "edge";
 
@@ -10,75 +10,79 @@ export async function GET(request: Request) {
     const hasTitle = searchParams.has("title");
     const title = hasTitle ? searchParams.get("title")?.slice(0, 100) : "My default title";
 
-    return new ImageResponse(
-      (
-        <div
-          style={{
-            display: "flex",
-            height: "100%",
-            width: "100%",
-            alignItems: "center",
-            justifyContent: "center",
-            letterSpacing: "-.02em",
-            fontWeight: 700,
-            background: "white",
-          }}>
-          <div
-            style={{
-              left: 42,
-              top: 42,
-              position: "absolute",
-              display: "flex",
-              alignItems: "center",
-            }}>
-            <span
-              style={{
-                width: 24,
-                height: 24,
-                background: "black",
-              }}
-            />
-            <span
-              style={{
-                marginLeft: 8,
-                fontSize: 20,
-              }}>
-              rauchg.com
-            </span>
-          </div>
-          <div
-            style={{
-              display: "flex",
-              flexWrap: "wrap",
-              justifyContent: "center",
-              padding: "20px 50px",
-              margin: "0 42px",
-              fontSize: 40,
-              width: "auto",
-              maxWidth: 550,
-              textAlign: "center",
-              backgroundColor: "black",
-              color: "white",
-              lineHeight: 1.4,
-            }}>
-            Navigate the job market with referrals
-          </div>
-        </div>
-      ),
-      {
-        width: 1200,
-        height: 630,
-      }
-    );
-  } catch (e: any) {
-    console.log(`${e.message}`);
-    return new Response(`Failed to generate the image`, {
-      status: 500,
-    });
+    return <></>;
+  } catch (log) {
+    console.log(log);
   }
 }
+// new ImageResponse(
+//       (
+//         <div
+//           style={{
+//             display: "flex",
+//             height: "100%",
+//             width: "100%",
+//             alignItems: "center",
+//             justifyContent: "center",
+//             letterSpacing: "-.02em",
+//             fontWeight: 700,
+//             background: "white",
+//           }}>
+//           <div
+//             style={{
+//               left: 42,
+//               top: 42,
+//               position: "absolute",
+//               display: "flex",
+//               alignItems: "center",
+//             }}>
+//             <span
+//               style={{
+//                 width: 24,
+//                 height: 24,
+//                 background: "black",
+//               }}
+//             />
+//             <span
+//               style={{
+//                 marginLeft: 8,
+//                 fontSize: 20,
+//               }}>
+//               rauchg.com
+//             </span>
+//           </div>
+//           <div
+//             style={{
+//               display: "flex",
+//               flexWrap: "wrap",
+//               justifyContent: "center",
+//               padding: "20px 50px",
+//               margin: "0 42px",
+//               fontSize: 40,
+//               width: "auto",
+//               maxWidth: 550,
+//               textAlign: "center",
+//               backgroundColor: "black",
+//               color: "white",
+//               lineHeight: 1.4,
+//             }}>
+//             Navigate the job market with referrals
+//           </div>
+//         </div>
+//       ),
+//       {
+//         width: 1200,
+//         height: 630,
+//       }
+//     );
+//   } catch (e: any) {
+//     console.log(`${e.message}`);
+//     return new Response(`Failed to generate the image`, {
+//       status: 500,
+//     });
+//   }
 
-// import { ImageResponse } from "@vercel/og";
+// ! import { ImageResponse } from "@vercel/og";
 
 // import { ogImageSchema } from "@/lib/validators";
 
