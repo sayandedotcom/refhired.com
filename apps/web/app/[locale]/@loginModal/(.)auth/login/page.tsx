d9bcccc7a6a6cc334ab29bf49aeb14c5abb41173
Sayan De <sayandeten@gmail.com>
2024-01-25 23:31:09 +0530

    error

--- apps/web/app/[locale]/@loginModal/(.)auth/login/page.tsx
+++ apps/web/app/[locale]/@loginModal/(.)auth/login/page.tsx
@@ -2,12 +2,16 @@
 
 import Login from "@/app/[locale]/(auth)/auth/login/page";
 
-import { Dialog, DialogContent, DialogTrigger } from "@referrer/ui";
+import {
+  Dialog,
+  DialogContent, //  DialogTrigger
+} from "@referrer/ui";
 
-export default function LoginModal({ children }: { children: any }) {
+export default function LoginModal() {
+  // { children }: { children: any }
   return (
     <Dialog>
-      <DialogTrigger>{children}</DialogTrigger>
+      {/* <DialogTrigger>{children}</DialogTrigger> */}
       <DialogContent className="border-foreground flex w-[450px] items-center justify-center p-1">
         <Login className="w-full border-none px-0" />
       </DialogContent>
