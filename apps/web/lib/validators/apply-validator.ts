e5bc7802b6997a3452f2f023d80000f129088ed4
Sayan De <sayandeten@gmail.com>
2024-02-04 20:30:44 +0530

    apply dialog form validator

--- apps/web/lib/validators/apply-validator.ts
+++ apps/web/lib/validators/apply-validator.ts
@@ -5,13 +5,34 @@ export const applyValidator = z.object({
     .string()
     .nonempty("Message is required")
     .max(300, { message: "Message must not be more than 300 characters." }),
-  resume: z.string().nonempty("Resume is required"),
-  coverLetter: z.string().nonempty("Cover Letter is required"),
-  urls: z
-    .array(
-      z.object({
-        value: z.string().url({ message: "Please enter a valid URL." }),
-      })
+  pdfs: z.array(
+    z.record(z.string().nonempty("Required by the Referrers").url({ message: "Please enter a valid URL." }))
+  ),
+  links: z.array(
+    z.record(
+      z
+        .string()
+        .min(1, { message: "This field cannot be empty." })
+        .nonempty("Required by the Referrers")
+        .url({ message: "Please enter a valid URL." })
     )
-    .optional(),
+  ),
 });
+// [key: string]: z.string().url({ message: "Please enter a valid URL." }),
+
+// urls: z
+//   .array(
+//     z.object({
+//       value: z.string().url({ message: "Please enter a valid URL." }),
+//     })
+//   )
+//   .optional(),
+// resume: z.string().nonempty("Resume is required").optional(),
+// coverLetter: z.string().nonempty("Cover Letter is required").optional(),
+// pdf: z
+//   .array(
+//     z.object({
+//       value: z.string().url({ message: "Please enter a valid URL." }),
+//     })
+//   )
+//   .optional(),
