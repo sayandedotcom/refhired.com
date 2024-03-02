2d08d14ce783922ec086b14f93ec46803308b619
Sayan De <sayandeten@gmail.com>
2024-03-02 22:07:03 +0530

    configuring serverless

--- apps/api/app.ts
+++ apps/api/app.ts
@@ -1,4 +1,5 @@
 import { ApolloServer } from "@apollo/server";
+import { handlers, startServerAndCreateLambdaHandler } from "@as-integrations/aws-lambda";
 import express from "express";
 
 import resolvers from "./graphql/resolvers.js";
@@ -7,10 +8,16 @@ import typeDefs from "./graphql/typeDefs.js";
 const app = express();
 
 const server = new ApolloServer({
-  typeDefs: typeDefs,
-  resolvers: resolvers,
-  introspection: true,
-  allowBatchedHttpRequests: true,
+  typeDefs,
+  resolvers,
 });
 
+export const graphqlHandler = startServerAndCreateLambdaHandler(
+  server as any,
+  // We will be using the Proxy V2 handler
+  handlers.createAPIGatewayProxyEventV2RequestHandler()
+);
 export { app, server };
+
+// introspection: true,
+// allowBatchedHttpRequests: true,
