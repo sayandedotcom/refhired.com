dd5ee35fcb1cec574160c2ba0e67a54dba77e64b
Sayan De <sayandeten@gmail.com>
2024-03-09 01:34:05 +0530

    adding read replica

--- packages/prisma/index.ts
+++ packages/prisma/index.ts
@@ -1,8 +1,15 @@
 import { PrismaClient } from "@prisma/client";
+import { readReplicas } from "@prisma/extension-read-replicas";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient().$extends(
+    readReplicas({
+      url: process.env.DATABASE_URL_REPLICA_1 as string,
+    })
+  );
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
