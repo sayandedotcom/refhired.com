6f051de91d0d06aad882095708850fb627a7898b
Sayan De <sayandeten@gmail.com>
2024-03-24 11:44:11 +0530

    removed data fetching

--- apps/web/app/[locale]/docs/page.tsx
+++ apps/web/app/[locale]/docs/page.tsx
@@ -1,29 +1,29 @@
 import { Metadata } from "next";
 
-import { Example } from "@/graphql/queries/posts";
+// import { Example } from "@/graphql/queries/posts";
 
-import { getClient } from "@/lib/apollo-client/client";
+// import { getClient } from "@/lib/apollo-client/client";
 
 export const metadata: Metadata = {
   title: "Docs",
   description: "Docs of Refhired.com",
 };
 
-const Docs = async () => {
-  const client = getClient();
-  const { data, loading, error, errors } = await client.query({
-    query: Example,
-  });
+const Docs = () => {
+  // const client = getClient();
+  // const { data, loading, error, errors } = await client.query({
+  //   query: Example,
+  // });
 
-  errors && <h1>errors.............</h1>;
-  error && <h1>error.............</h1>;
+  // errors && <h1>errors.............</h1>;
+  // error && <h1>error.............</h1>;
   return (
     <section className="py-14">
-      {loading ? (
+      {/* {loading ? (
         <h1>Loading.............</h1>
       ) : (
         data?.getTodos.map(({ title }, i) => <h2 key={i}>{title}</h2>)
-      )}
+      )} */}
 
       <div className="relative mx-auto max-w-xl sm:text-center">
         <h1 className="font-heading font-semibold">Our Docs</h1>
