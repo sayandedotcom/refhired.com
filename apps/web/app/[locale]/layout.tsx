05ff7aaf69ee73e0e1caa1ebe427df687b35ef4b
Sayan De <sayandeten@gmail.com>
2024-03-31 00:53:42 +0530

    removed unstable_setRequestLocale to see if parallel routing works

--- apps/web/app/[locale]/layout.tsx
+++ apps/web/app/[locale]/layout.tsx
@@ -4,7 +4,6 @@ import localFont from "next/font/local";
 
 import { locales } from "@/navigation";
 import { NextIntlClientProvider, useMessages } from "next-intl";
-import { unstable_setRequestLocale } from "next-intl/server";
 
 import { siteConfig } from "@/config";
 
@@ -98,7 +97,7 @@ export default function RootLayout({
 }) {
   console.log("params---locale=====================", locale);
 
-  unstable_setRequestLocale(locale);
+  // unstable_setRequestLocale(locale);
 
   const messages = useMessages();
   return (
