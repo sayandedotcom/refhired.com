cdba6036fae5e0dcd674bcbaf2b9b423cc42e1b5
Sayan De <sayandeten@gmail.com>
2024-03-31 13:46:02 +0530

    added default pages in settings tab

---
+++ apps/web/app/[locale]/(portals)/settings/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from "next";
+
+import { redirect } from "@/navigation";
+import { unstable_setRequestLocale } from "next-intl/server";
+
+export const metadata: Metadata = {
+  title: "Settings",
+  description: "Settings of Refhired.com",
+};
+
+export default function Settings({ params: { locale } }) {
+  unstable_setRequestLocale(locale);
+  return redirect("/settings/profile");
+}
