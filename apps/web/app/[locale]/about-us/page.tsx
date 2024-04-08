cfa1a8c7cf4bf80e4ef22a23fd8feba452c58401
Sayan De <sayandeten@gmail.com>
2024-04-08 21:01:53 +0530

    add

--- apps/web/app/[locale]/about-us/page.tsx
+++ apps/web/app/[locale]/about-us/page.tsx
@@ -11,10 +11,16 @@ const AboutUs = ({ params: { locale } }) => {
   unstable_setRequestLocale(locale);
   return (
     <section className="py-14">
-      <div className="relative mx-auto max-w-xl sm:text-center">
+      <div className="relative mx-auto max-w-3xl sm:text-center">
         <h1 className="font-heading font-semibold">About Us</h1>
-        <div className="mt-3 max-w-xl">
-          <h5 className="font-heading">Will be Updated soon</h5>
+        <div className="mx-auto mt-3">
+          <h5 className="font-heading">
+            [Temporary Notice] Initially, this website&apos;s backend was built with Next.js 13 server
+            actions. However, due to scalability, security, and limited community support concerns, I've opted
+            to migrate the entire backend to Node.js, Express.js, and GraphQL. This migration process may take
+            some time, during which certain services may be unavailable. Additionally, I am transitioning the
+            deployment platform from Vercel to AWS. I plan to fully redeploy this website by May 1st.
+          </h5>
         </div>
       </div>
     </section>
