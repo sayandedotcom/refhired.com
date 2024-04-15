8721655f6735e56281290cc14adc78358258670e
Sayan De <sayandeten@gmail.com>
2024-04-15 18:22:35 +0530

    add

---
+++ stacks/corn.ts
@@ -0,0 +1,8 @@
+// import { Cron, StackContext } from "sst/constructs";
+
+// export function APP({ stack }: StackContext) {
+//   new Cron(stack, "cron", {
+//     schedule: "rate(1 day)",
+//     job: "packages/functions/src/cron.handler",
+//   });
+// }
