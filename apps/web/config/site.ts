215607839c40e44493ef1146511d5b3c2101fe9f
Sayan De <sayandeten@gmail.com>
2024-05-11 23:47:49 +0530

    site config

--- apps/web/config/site.ts
+++ apps/web/config/site.ts
@@ -1,15 +1,19 @@
 import { SiteConfig } from "@/types";
 
-const siteUrl = process.env.NEXT_PUBLIC_APP_URL || "https://chadnext.moinulmoin.com";
+// const siteUrl = process.env.NEXT_PUBLIC_APP_URL || "https://refhired.sayande.com";
+
+const siteUrl =
+  (process.env.NODE_ENV === "development" && "http://localhost:3000") ||
+  (process.env.NODE_ENV === "production" && "https://refhired.sayande.com");
 
 export const siteConfig: SiteConfig = {
   name: "Refhired.com",
   description: "Get job referrals from employees of top best companies of the world.",
   url: siteUrl,
-  ogImage: "https://tx.shadcn.com/og.jpg",
+  ogImage: "https://refhired.sayande.com/api/og",
   email: "sayandeten@gmail.com", // ! noreply@refhired.com
   links: {
-    twitter: "https://twitter.com/sayande2002",
-    github: "https://github.com/shadcn/taxonomy",
+    twitter: "https://twitter.com/sayandedotcom",
+    github: "https://github.com/sayandedotcom/refhired.com",
   },
 };
