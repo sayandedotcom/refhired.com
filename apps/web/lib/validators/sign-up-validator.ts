9f6519a497dd6ff8fe305845159e9e78e85c86ba
Sayan De <sayandeten@gmail.com>
2024-05-11 23:50:15 +0530

    signup validator

--- apps/web/lib/validators/sign-up-validator.ts
+++ apps/web/lib/validators/sign-up-validator.ts
@@ -6,11 +6,11 @@ export const signUpValidator = z.object({
     message: "Username must be at least 5 characters.",
   }),
   email: z.string().email({ message: "Invalid email address" }).nonempty("Your email is required"),
-  // password: z.string().nonempty("Your password is required").min(8, {
-  //   message: "Password must be at least 8 characters.",
-  // }),
-  // confirmPassword: z.string().nonempty("Confirm Password is required"),
 });
+// password: z.string().nonempty("Your password is required").min(8, {
+//   message: "Password must be at least 8 characters.",
+// }),
+// confirmPassword: z.string().nonempty("Confirm Password is required"),
 // .refine((data) => data.password === data.confirmPassword, {
 //   message: "Password doesn't match",
 //   path: ["confirmPassword"],
