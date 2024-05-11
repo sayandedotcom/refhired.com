871f76d00e88c10d57f3dab7691d4216a2799b97
Sayan De <sayandeten@gmail.com>
2024-05-11 23:52:57 +0530

    share dialog component

---
+++ apps/web/components/dialog/share-dialog.tsx
@@ -0,0 +1,144 @@
+"use client";
+
+import { Copy } from "lucide-react";
+import { useLocale } from "next-intl";
+import {
+  EmailIcon,
+  EmailShareButton,
+  FacebookIcon,
+  FacebookShareButton,
+  LinkedinIcon,
+  LinkedinShareButton,
+  RedditIcon,
+  RedditShareButton,
+  TwitterShareButton,
+  WhatsappIcon,
+  WhatsappShareButton,
+  XIcon,
+} from "react-share";
+import { useCopyToClipboard } from "usehooks-ts";
+
+import {
+  Button,
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  Input,
+  Label,
+} from "@referrer/ui";
+
+import { siteConfig } from "@/config";
+
+import { sonerToast } from "../ui";
+
+export function ShareDialog({
+  children,
+  shareUrl,
+  title,
+}: {
+  children: React.ReactNode;
+  shareUrl?: string;
+  title?: string;
+}) {
+  const [copiedText, copy] = useCopyToClipboard();
+
+  const locale = useLocale();
+
+  const url = `${siteConfig.url}/${locale}/${shareUrl}`;
+
+  const size = 50;
+
+  const handleCopy = (text: string) => () => {
+    copy(text)
+      .then(() => {
+        console.log("Copied!", { text });
+        sonerToast({
+          title: "Copied to your clipboard !",
+          severity: "neutral",
+        });
+      })
+      .catch((error) => {
+        console.error("Failed to copy!", error);
+      });
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>{children}</DialogTrigger>
+      <DialogContent className="sm:max-w-md">
+        <DialogHeader>
+          <DialogTitle>Share !</DialogTitle>
+          <DialogDescription>Share on your Social Media sitess</DialogDescription>
+        </DialogHeader>
+        <div className="scrollbar-hide flex gap-4 overflow-hidden overflow-x-scroll">
+          <div className="Demo__some-network">
+            <WhatsappShareButton
+              url={url}
+              title={title}
+              separator=":: "
+              className="Demo__some-network__share-button">
+              <WhatsappIcon size={size} round />
+            </WhatsappShareButton>
+          </div>
+          <div className="Demo__some-network">
+            <TwitterShareButton url={url} title={title} className="Demo__some-network__share-button">
+              <XIcon size={size} round />
+            </TwitterShareButton>
+          </div>
+          <div className="Demo__some-network">
+            <FacebookShareButton url={url} className="Demo__some-network__share-button">
+              <FacebookIcon size={size} round />
+            </FacebookShareButton>
+          </div>
+          <div className="Demo__some-network">
+            <EmailShareButton
+              url={url}
+              subject={title}
+              body="body"
+              className="Demo__some-network__share-button">
+              <EmailIcon size={size} round />
+            </EmailShareButton>
+          </div>
+          <div className="Demo__some-network">
+            <LinkedinShareButton url={url} className="Demo__some-network__share-button">
+              <LinkedinIcon size={size} round />
+            </LinkedinShareButton>
+          </div>
+          <div className="Demo__some-network">
+            <RedditShareButton
+              url={url}
+              title={title}
+              windowWidth={660}
+              windowHeight={460}
+              className="Demo__some-network__share-button">
+              <RedditIcon size={size} round />
+            </RedditShareButton>
+          </div>
+          <div className="Demo__some-network">
+            <LinkedinShareButton url={url} className="Demo__some-network__share-button">
+              <LinkedinIcon size={size} round />
+            </LinkedinShareButton>
+          </div>
+        </div>
+        <DialogDescription>Anyone who has this link will be able to view this.</DialogDescription>
+        <div className="flex items-center space-x-2">
+          <div className="grid flex-1 gap-2">
+            <Label htmlFor="link" className="sr-only">
+              Link
+            </Label>
+            <Input id="link" defaultValue={url} readOnly />
+          </div>
+          <Button type="submit" size="sm" className="px-3" onClick={handleCopy(url)}>
+            <span className="sr-only">Copy</span>
+            <Copy className="h-4 w-4" />
+          </Button>
+        </div>
+        <DialogFooter className="sm:justify-start"></DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
