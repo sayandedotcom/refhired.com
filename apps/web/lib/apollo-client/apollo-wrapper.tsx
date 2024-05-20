5432af811ef431949bc4785010a7750392cc3460
Sayan De <sayandeten@gmail.com>
2024-05-21 01:52:09 +0530

    added new Apollo wrapper

--- apps/web/lib/apollo-client/apollo-wrapper.tsx
+++ apps/web/lib/apollo-client/apollo-wrapper.tsx
@@ -8,44 +8,105 @@ import {
   NextSSRInMemoryCache,
   SSRMultipartLink,
 } from "@apollo/experimental-nextjs-app-support/ssr";
-import { getCookie } from "cookies-next";
+import { readSSROnlySecret } from "ssr-only-secrets";
 
-function makeClient() {
-  const httpLink = new HttpLink({
-    uri: "http://localhost:8000/graphql",
+const getAuthLink = (cloakedSessionId: Promise<string>) =>
+  setContext(async (_, { headers }) => {
+    const sessionId = await readSSROnlySecret(
+      // @ts-ignore
+      await cloakedSessionId,
+      "SECRET_KEY_VAR"
+    );
+
+    console.log(
+      "2st Console run======================================================================================================================================"
+    );
+
+    if (sessionId) {
+      return {
+        headers: {
+          ...(headers || {}),
+          Cookie: `sessionid=${sessionId}`,
+          // ...headers,
+          // authorization: sessionId ? `Bearer from wrapper ${sessionId}` : "",
+        },
+      };
+    }
+
+    return { headers };
   });
 
-  const authLink = setContext((_, { headers }) => {
-    const token = getCookie("__Secure-next-auth.session-token") ?? getCookie("next-auth.session-token");
-
-    console.log("token====================", token);
-
-    return {
-      headers: {
-        ...headers,
-        authorization: token ? `Bearer ${token}` : "",
+export function ApolloWrapper({
+  children,
+  sessionId,
+}: React.PropsWithChildren<{ sessionId: Promise<string> }>) {
+  function makeClient() {
+    const httpLink = new HttpLink({
+      uri: typeof window === "undefined" ? "http://localhost:8000/graphql" : "http://localhost:8000/graphql",
+      fetchOptions: {
+        cache: "no-store",
+        crendentials: "include",
       },
-    };
-  });
+    });
 
-  return new NextSSRApolloClient({
-    cache: new NextSSRInMemoryCache(),
-    link:
-      typeof window === "undefined"
-        ? ApolloLink.from([
-            new SSRMultipartLink({
-              stripDefer: true,
-            }),
-            authLink.concat(httpLink),
-          ])
-        : authLink.concat(httpLink),
-  });
-}
+    console.log(
+      "1st Console run======================================================================================================================================"
+    );
 
-export function ApolloWrapper({ children }: React.PropsWithChildren) {
+    return new NextSSRApolloClient({
+      cache: new NextSSRInMemoryCache(),
+      link:
+        typeof window === "undefined"
+          ? ApolloLink.from([
+              new SSRMultipartLink({
+                stripDefer: true,
+              }),
+              getAuthLink(sessionId),
+              httpLink,
+            ])
+          : httpLink,
+    });
+  }
+
   return <ApolloNextAppProvider makeClient={makeClient}>{children}</ApolloNextAppProvider>;
 }
-// const appLink = from([errorLink, httpLink]);
+
+// function makeClient() {
+//   const httpLink = new HttpLink({
+//     uri: "http://localhost:8000/graphql",
+//   });
+
+//   const authLink = setContext((_, { headers }) => {
+//     const token = getCookie("__Secure-next-auth.session-token") ?? getCookie("next-auth.session-token");
+
+//     console.log("token====================", token);
+
+//     return {
+//       headers: {
+//         ...headers,
+//         authorization: token ? `Bearer ${token}` : "",
+//       },
+//     };
+//   });
+
+//   return new NextSSRApolloClient({
+//     cache: new NextSSRInMemoryCache(),
+//     link:
+//       typeof window === "undefined"
+//         ? ApolloLink.from([
+//             new SSRMultipartLink({
+//               stripDefer: true,
+//             }),
+//             authLink.concat(httpLink),
+//           ])
+//         : authLink.concat(httpLink),
+//   });
+// }
+
+// export function ApolloWrapper({ children }: React.PropsWithChildren) {
+//   return <ApolloNextAppProvider makeClient={makeClient}>{children}</ApolloNextAppProvider>;
+// }
+// ! const appLink = from([errorLink, httpLink]);
 // const errorLink = onError(({ graphQLErrors, networkError }) => {
 //   if (graphQLErrors) {
 //     console.log("graphQLErrorsclient------------------------------", graphQLErrors);
