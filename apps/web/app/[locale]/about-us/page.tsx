31ddeec330e9730a51dcd241838b740ab31a065a
Sayan De <sayandeten@gmail.com>
2024-05-21 19:22:03 +0530

    updated notice

--- apps/web/app/[locale]/about-us/page.tsx
+++ apps/web/app/[locale]/about-us/page.tsx
@@ -12,16 +12,17 @@ const AboutUs = ({ params: { locale } }) => {
   return (
     <section className="py-14">
       <div className="relative mx-auto max-w-3xl sm:text-center">
-        <h1 className="font-heading font-semibold">About Us</h1>
+        <h1 className="font-heading font-semibold">Temporary Notice</h1>
         <div className="mx-auto mt-3">
           <h5 className="font-heading">
-            [Temporary Notice] Initially, this website's backend was built with Next.js 13/14 server actions.
-            However, due to scalability, security, and limited community support concerns, I've opted to
-            migrate the entire backend to Node.js and Express.js, utilizing a GraphQL API. This migration
-            process may take some time, during which certain services may be unavailable. Additionally, I am
-            transitioning the deployment platform from Vercel to AWS to gain more control over resources and
-            reduce costs, with the assistance of the Serverless Stack (SST) framework. I plan to fully
-            redeploy this website by May 1st. Thank you to all the visitors!
+            Initially, this website's backend was built with Next.js 13/14 server actions. However, due to
+            scalability, security, and limited community support concerns, I've opted to migrate the entire
+            backend to Node.js and Express.js, utilizing a GraphQL API. This migration process may take some
+            time, during which all services will be unavailable.
+            <br />
+            Additionally, I am transitioning the deployment platform from Vercel to AWS to gain more control
+            over resources and reduce costs, with the assistance of the Serverless Stack (SST) framework. I
+            plan to fully redeploy this website by late June. Thank you to all the visitors!
           </h5>
         </div>
       </div>
