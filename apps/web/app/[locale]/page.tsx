cfb26da02f3d67476fe0333d03db9684417bfd4a
Sayan De <sayandeten@gmail.com>
2024-05-26 23:44:05 +0530

    added streaming

--- apps/web/app/[locale]/page.tsx
+++ apps/web/app/[locale]/page.tsx
@@ -36,35 +36,37 @@ export default function Page({ params: { locale } }) {
         <TopNotificationList className="fixed right-8 top-16 z-40 hidden lg:block" />
       </Suspense>
 
-      <div className="mt-3 flex flex-col items-center justify-center gap-10 lg:p-16">
-        {/* <Notice arrow href="/about-us">
+      <Suspense fallback={<Loading />}>
+        <div className="mt-3 flex flex-col items-center justify-center gap-10 lg:p-16">
+          {/* <Notice arrow href="/about-us">
           Important Notice !
         </Notice> */}
-        <Infotmation />
-        <h1 className="font-heading px-1 text-center text-[36px] dark:bg-gradient-to-r dark:from-[#abbaab] dark:to-[#ffffff] dark:bg-clip-text dark:text-transparent lg:text-[86px]">
-          " Navigate the job market with{" "}
-          <span className="bg-gradient-to-r from-pink-500 to-yellow-500  bg-clip-text text-transparent">
-            {t("referrals")}
-          </span>{" "}
-          :{" "}
-          <span className="">
-            Connect through{" "}
-            <span className="bg-gradient-to-r from-green-400 to-blue-500 bg-clip-text text-transparent">
-              referrals
+          <Infotmation />
+          <h1 className="font-heading px-1 text-center text-[36px] dark:bg-gradient-to-r dark:from-[#abbaab] dark:to-[#ffffff] dark:bg-clip-text dark:text-transparent lg:text-[86px]">
+            " Navigate the job market with{" "}
+            <span className="bg-gradient-to-r from-pink-500 to-yellow-500  bg-clip-text text-transparent">
+              {t("referrals")}
             </span>{" "}
-          </span>
-          "
-        </h1>
-        {/* <p className="px-6 text-center text-[15px] font-medium leading-normal sm:text-lg sm:leading-7 md:mt-7 md:px-28 md:text-[20px]">
+            :{" "}
+            <span className="">
+              Connect through{" "}
+              <span className="bg-gradient-to-r from-green-400 to-blue-500 bg-clip-text text-transparent">
+                referrals
+              </span>{" "}
+            </span>
+            "
+          </h1>
+          {/* <p className="px-6 text-center text-[15px] font-medium leading-normal sm:text-lg sm:leading-7 md:mt-7 md:px-28 md:text-[20px]">
           Used by many job seekers and employees of the world's largest companies, Referrer enables you to get
           job through referrals which has the highest convertion rates among other methods . It simplifies the
           tasks for both job seekers and employees who gives referrals
         </p> */}
-        <Suspense fallback={<Loading />}>
-          <JoinWaitlist />
-        </Suspense>
-        <ShimmerButtonComponent href={"/home"}>Explore the MVP !</ShimmerButtonComponent>
-      </div>
+          <Suspense fallback={<Loading />}>
+            <JoinWaitlist />
+          </Suspense>
+          <ShimmerButtonComponent href={"/home"}>Explore the MVP !</ShimmerButtonComponent>
+        </div>
+      </Suspense>
 
       <Suspense fallback={<Loading />}>
         <Companies />
