359b7aa2e0fdafc641ab1a2da04b023447ca7e36
Sayan De <sayandeten@gmail.com>
2024-09-15 11:54:48 +0530

    changed data fetching in home route

--- apps/web/app/[locale]/(portals)/home/page.tsx
+++ apps/web/app/[locale]/(portals)/home/page.tsx
@@ -7,23 +7,23 @@ import { PostCard } from "@/components/custom-components";
 import { MultipleButtons } from "@/components/custom-components/post-card/post-buttons";
 import Navigate from "@/components/navigate";
 
-import { requests } from "@/lib/axios";
-
 export const metadata: Metadata = {
   title: "Home",
   description: "Get job referrals to the top best companies of the world",
 };
 
-const Home = async ({ params: { locale } }) => {
+async function Home({ params: { locale } }) {
   unstable_setRequestLocale(locale);
-  const response = await requests
-    .get("posts", {
-      headers: {
-        name: "Sayan De from Server Component",
-      },
-    })
-    .then((response) => response.data)
-    .catch((error) => console.log(error));
+  const response = await fetch("http://localhost:3000/api/v1/posts", {
+    method: "GET",
+  }).then((response) => response.json());
+  // .get("posts", {
+  //   headers: {
+  //     name: "Sayan De from Server Component",
+  //   },
+  // })
+  // .then((response) => response.data)
+  // .catch((error) => console.log(error));
 
   console.log("😊Postsssssssssssssssssssssssssssss", response);
   const posts: any[] = [];
@@ -67,6 +67,6 @@ const Home = async ({ params: { locale } }) => {
       ))}
     </>
   );
-};
+}
 
 export default Home;
