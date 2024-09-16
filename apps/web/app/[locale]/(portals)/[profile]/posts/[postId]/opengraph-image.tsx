169b406c755d48ffacc6a5c5708b1ffb0e925c54
Sayan De <sayandeten@gmail.com>
2024-09-16 23:12:52 +0530

    add postsId route

---
+++ apps/web/app/[locale]/(portals)/[profile]/posts/[postId]/opengraph-image.tsx
@@ -0,0 +1,32 @@
+export const size = {
+  width: 900,
+  height: 450,
+};
+
+export const contentType = "image/png";
+
+interface Props {
+  params: {
+    slug: number;
+  };
+}
+
+export default async function og({ params }: Props) {
+  // const post =  getPostBySlug(params.slug);
+
+  return null;
+}
+//  new ImageResponse(
+//   (
+//     <div tw="relative flex items-center justify-center">
+//       <Image src={post?.image} alt="post" />
+//       <div tw="absolute flex bg-black opacity-50 inset-0 " />
+//       <div tw="absolute flex items-center top-2 w-full ">
+//         <p tw="text-white text-4xl flex font-bold m-5">{post?.description}</p>
+//         <p tw="text-indigo-200 text-xl flex font-bold m-5">{post?.user.userName}</p>
+//         <p tw="text-purple-200 text-xl flex font-bold m-5">{post?.updatedAt?.toDateString()}</p>
+//       </div>
+//     </div>
+//   ),
+//   size
+// );
---
+++ apps/web/app/[locale]/(portals)/[profile]/posts/[postId]/page.tsx
@@ -0,0 +1,79 @@
+import { Metadata } from "next";
+
+import { fromNow } from "@refhiredcom/utils";
+
+import { PostCard } from "@/components/custom-components";
+import { MultipleButtons } from "@/components/custom-components/post-card/post-buttons";
+
+import { request } from "@/lib/axios";
+
+interface PostProps {
+  params: {
+    postId: string;
+  };
+}
+
+// export async function generateMetadata({ params }: PostProps): Promise<Metadata> {
+//   const post = await getPostBySlug(params.postId);
+//   if (!post)
+//     return {
+//       title: "Not Found",
+//       description: "The page is not found",
+//     };
+
+//   return {
+//     title: post.title,
+//     description: post.description,
+//     alternates: {
+//       canonical: `/post/${post.id}`, // ! Canonical url for the post
+//       languages: {
+//         "en-CA": `en-CA/post/${post.id}`, // ! English (Canada) language website
+//       },
+//     },
+//   };
+// }
+
+export const metadata: Metadata = {
+  title: "Posts",
+  description: "Get job referrals to the top best companies of the world",
+};
+
+export default async function Post({ params }: PostProps) {
+  const { data } = (await request.get(`/posts/${params.postId}`)).data;
+
+  return (
+    <>
+      <PostCard key={data.id}>
+        <PostCard.Image src={data?.user?.image ?? "/images/avatar/avatar.png"} />
+        <PostCard.Content>
+          <PostCard.Header
+            name={data.user?.name}
+            userName={`@${data.user?.userName}`}
+            time={fromNow(data.createdAt)}
+            timeLeft={fromNow(data.expiresAt)}
+          />
+          <PostCard.Description>{data.description}</PostCard.Description>
+          <PostCard.Tags
+            allTags={true}
+            location={data.jobLocation}
+            experience={data.jobExperience}
+            jobType={data.jobType}
+            role={data.jobRole}
+            salary={data.jobCompensation}
+            skills={data.tags}
+          />
+          <PostCard.Footer>
+            <MultipleButtons />
+            {/* <ApplyDialog
+              myObject={data.getPostBySlug.accept}
+              postID={data.getPostBySlug.id}
+              stars={data.getPostBySlug.stars}
+              totalApplied={data.getPostBySlug.totalApplied}
+              acceptLimit={data.getPostBySlug.acceptLimit}
+            /> */}
+          </PostCard.Footer>
+        </PostCard.Content>
+      </PostCard>
+    </>
+  );
+}
