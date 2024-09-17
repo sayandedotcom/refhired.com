e3a4974ee65c3f06e069061c5d479557c6544e35
Sayan De <sayandeten@gmail.com>
2024-09-18 00:14:14 +0530

    added waitlist route

--- apps/web/app/api/v1/waitlist/route.ts
+++ apps/web/app/api/v1/waitlist/route.ts
@@ -1,22 +1,20 @@
+import { type NextRequest, NextResponse } from "next/server";
+
 import prisma from "@referrer/prisma";
 
-export async function POST(req: Request) {
+export async function POST(req: NextRequest) {
   try {
     const { email } = await req.json();
     if (!email) {
-      return Response.json({ message: "Please enter your email address.", status: 400 }, { status: 400 });
+      return NextResponse.json({ message: "Please enter your email address." }, { status: 400 });
     }
     const emailExists = await prisma.user.findFirst({
       where: {
         email,
       },
     });
-
     if (emailExists) {
-      return Response.json(
-        { message: "Your email is already in our waitlist.", status: 409 },
-        { status: 409 }
-      );
+      return NextResponse.json({ message: "Your email is already in our waitlist." }, { status: 409 });
     } else {
       await prisma.user.create({
         data: {
@@ -24,10 +22,7 @@ export async function POST(req: Request) {
           email,
         },
       });
-      return Response.json(
-        { message: `Sucessfully added to wistlist ${email}`, status: 200 },
-        { status: 200 }
-      );
+      return NextResponse.json({ message: `Sucessfully added to wistlist ${email}` }, { status: 200 });
     }
   } catch (error) {
     return Response.json({ message: `${error?.message}` }, { status: 404 });
