84b2be172edfa298e93da32c5fd5eca6477b85d2
Sayan De <sayandeten@gmail.com>
2024-09-18 16:43:19 +0530

    update

--- apps/web/components/custom-components/portal-components/portal-not-found.tsx
+++ apps/web/components/custom-components/portal-components/portal-not-found.tsx
@@ -7,7 +7,7 @@ import { Button } from "@referrer/ui";
 export const PortalsNotFound = ({ text }) => {
   return (
     <div className="mt-8 flex flex-col items-center justify-center gap-5">
-      <p>You have no {text} !</p>
+      <p>🤔 Ooops ! You have no {text} !</p>
       <Button className="rounded-full">
         <RotateCcw className="mr-2 h-4 w-4" />
         Retry
