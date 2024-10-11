cc62a94c6bd6df6249039c947a69e46e548d180e
Sayan De <sayandeten@gmail.com>
2024-10-11 13:14:46 +0530

    add

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -42,32 +42,14 @@ module.exports = withNextIntl(
     async rewrites() {
       return [
         {
-          source: "/blog",
-          destination: `${process.env.DASHBOARD_URL}/blog`,
-        },
-        {
-          source: "/:locale*/demodash22/:path+",
-          destination: `${process.env.DASHBOARD_URL}/:path+`,
-        },
-        {
           source: "/:locale*/demodash1",
-          destination: `https://dashboard.refhired.com/?tab=overview`,
+          destination: `${process.env.DASHBOARD_URL}/:locale*`,
+          basePath: false,
         },
         {
-          source: "/:locale*/demodash2/:path+",
-          destination: `https://dashboard.refhired.com/:path+`,
-        },
-        {
-          source: `/en/demodash23/:path+`,
-          destination: `https://dashboard.refhired.com/:path+`,
-        },
-        {
-          source: `/en/demodash3/:path+`,
-          destination: `https://dashboard.refhired.com/:path+`,
-        },
-        {
-          source: "/:locale*/demo10/:match*",
-          destination: "https://dashboard.refhired.com/:match*",
+          source: "/:locale*/demodash1/:path+",
+          destination: `${process.env.DASHBOARD_URL}/:locale*/:path+`,
+          basePath: false,
         },
       ];
     },
