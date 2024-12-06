0f17c1c7a539672f59aaf2d5af724beae30fc0e1
Sayan De <sayandeten@gmail.com>
2024-12-07 00:23:25 +0530

    add

--- apps/web/app/api/stripe/route.ts
+++ apps/web/app/api/stripe/route.ts
@@ -3,6 +3,8 @@ import { NextRequest, NextResponse } from "next/server";
 import { auth } from "@/lib/auth";
 import { stripe } from "@/lib/stripe";
 
+import { siteConfig } from "@/config";
+
 export async function POST(req: NextRequest) {
   // const billingUrl = siteConfig(locale).url + "/dashboard/billing/";
   const { planId = process.env.STRIPE_CUST0M_PRICE_ID, quantity = 1, stars } = await req.json();
@@ -29,8 +31,8 @@ export async function POST(req: NextRequest) {
         userId: user.id,
         stars,
       },
-      success_url: "http://localhost:3000/purchase",
-      cancel_url: "http://localhost:3000/purchase",
+      success_url: `${siteConfig.url}/purchase`,
+      cancel_url: `${siteConfig.url}/purchase`,
     });
 
     return new NextResponse(JSON.stringify({ url: checkoutSession.url, stars: stars }));
