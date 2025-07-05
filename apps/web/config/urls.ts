2b46bdd8af03536acbcaf25d638efc6dbd4823eb
Sayan De <sayandeten@gmail.com>
2025-07-06 03:31:50 +0530

    refractor: changed urt name and methods

--- apps/web/config/urls.ts
+++ apps/web/config/urls.ts
@@ -1,6 +1,6 @@
 const DEV_API_URL: string = "http://localhost:3000/api";
 
-const PROD_API_URL: string = "https://refhired.com/api";
+const PROD_API_URL: string = "https://refhired.sayande.com/api";
 
 const API_URL: string = process.env.NODE_ENV === "development" ? DEV_API_URL : PROD_API_URL;
 
--- apps/web/lib/axios.ts
+++ apps/web/lib/axios.ts
@@ -5,10 +5,6 @@ export const request = axios.create({
   baseURL: `${API_URL}/v1`,
   headers: {
     "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": DEV_API_URL,
-    PROD_API_URL,
-    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-    "Access-Control-Allow-Headers": "Content-Type, Authorization",
   },
   params: {
     key: REFHIREDCOM_API_KEY,
