f8061f92e597887c4bce1faea688a127645a634a
Sayan De <sayandeten@gmail.com>
2025-07-06 23:44:37 +0530

    refactor: changed api version

--- apps/web/lib/axios.ts
+++ apps/web/lib/axios.ts
@@ -1,8 +1,9 @@
 import { API_URL, DEV_API_URL, PROD_API_URL, REFHIREDCOM_API_KEY } from "@/config/urls";
 import axios from "axios";
 
+// baseURL: `${API_URL}/v1`,
 export const request = axios.create({
-  baseURL: `${API_URL}/v1`,
+  baseURL: "/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
