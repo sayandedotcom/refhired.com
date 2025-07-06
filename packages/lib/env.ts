5f769f61d722093938753d1d7b7e461d7e6c2189
Sayan De <sayandeten@gmail.com>
2025-07-06 21:53:46 +0530

    feat: added shared lib package

---
+++ packages/lib/env.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+const envSchema = z.object({
+  // REDIS_URI: z.string().url(),
+  // JWT_SECRET: z.string().min(1),
+  // ABLY_API_KEY: z.string().min(1),
+  NODE_ENV: z.enum(["development", "staging", "production"]).optional(),
+});
+
+function createEnv(env: NodeJS.ProcessEnv) {
+  const safeParseResult = envSchema.safeParse(env);
+  if (!safeParseResult.success) throw new Error(safeParseResult.error.message);
+  return safeParseResult.data;
+}
+
+export const env = createEnv(process.env);
