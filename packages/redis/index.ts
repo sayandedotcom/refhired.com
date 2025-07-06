a3e77746337b8e4dce75da295b45dc55e889d7f9
Sayan De <sayandeten@gmail.com>
2025-07-06 13:03:23 +0530

    refractor: removed redis

--- packages/redis/index.ts
+++
@@ -1,9 +0,0 @@
-import { Redis } from "ioredis";
-
-const redisClient = new Redis(process.env.VERCEL_KV_URL!);
-
-redisClient.on("error", (error) => {
-  console.error("Redis error:", error);
-});
-
-export default redisClient;
