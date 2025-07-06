c4fe0798c5476c429b6f611f0f68b8bdb173ab07
Sayan De <sayandeten@gmail.com>
2025-07-06 21:52:44 +0530

    feat: shared types package

---
+++ packages/types/src/post-types.d.ts
@@ -0,0 +1,53 @@
+import type { Posts, Tags, User } from "@prisma/client";
+import {
+  applyValidator,
+  findReferrerValidator,
+  postValidator,
+  referralPostValidator,
+} from "@refhiredcom/validators";
+import { z } from "zod";
+
+export type TReferralPost = z.infer<typeof referralPostValidator>;
+
+export type TFindReferralPost = z.infer<typeof findReferrerValidator>;
+
+export type TPost = z.infer<typeof postValidator>;
+
+export type TApplyPost = z.infer<typeof applyValidator>;
+
+export type TPostReferralPost = {
+  accept: Posts["accept"];
+  acceptLimit: Posts["acceptLimit"];
+  companyName: Posts["companyName"];
+  description: Posts["description"];
+  expiresAt: Posts["expiresAt"];
+  jobCode: Posts["jobCode"];
+  jobCompensation: Posts["jobCompensation"];
+  jobExperience: Posts["jobExperience"];
+  jobLocationType: Posts["jobLocationType"];
+  jobLocation: Posts["jobLocation"];
+  jobRole: Posts["jobRole"];
+  jobType: Posts["jobType"];
+  jobURL: Posts["jobURL"];
+  postType: Posts["postType"];
+  stars: Posts["stars"];
+  tags: string[];
+};
+
+export type TReturnedPostsData = Posts & { user: User };
+
+export type TPosts = {
+  data: {
+    data: TReturnedPostsData[];
+  };
+};
+
+export type TPostsData = {
+  data: { data: Posts & { user: User; tags: Tags[] } };
+};
+
+export type TProfile = { data: { data: User & { posts: Posts[] } } };
+
+export type TSettingsProfile = { data: User };
+
+export type TApply = { applyInfo: any; starsRequired?: number; authorId: string };
